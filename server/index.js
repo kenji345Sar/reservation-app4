c47c3c9f332296209a7ebd2e5896fa4414e4b452
kenjiSar <tsawada@mbj.nifty.com>
2020-07-16 20:46:06 +0900

    Herokuデプロイ

--- server/index.js
+++ server/index.js
@@ -2,20 +2,21 @@ var express = require("express");
 var app = express();
 
 const mongoose = require('mongoose');
-const config = require('./config/dev')
+const config = require('./config')
 const SampleDb = require('./sample-db')
 const productRoutes = require('./routes/products')
-
+const path = require('path')
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(
   () => {
+    if(process.env.NODE_ENV !== 'production') {
       const sampleDb = new SampleDb()
-      sampleDb.initDb()
-      // sampleDb.pushProductsToDb()
+      // sampleDb.initDb()
     }
+  }
 )
 
 
@@ -24,7 +25,13 @@ mongoose.connect(config.DB_URI, {
 // })
 app.use('/api/v1/products', productRoutes)
 
-
+if(process.env.NODE_ENV === 'production') {
+  const appPath = path.join( __dirname, '..', 'dist', 'reservation-app4')
+  app.use(express.static(appPath))
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
 var server = app.listen(3000, function(){

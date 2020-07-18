51919f7ef4f361953a20c1f6bb581fb2d630b565
kenjiSar <tsawada@mbj.nifty.com>
2020-07-18 15:26:10 +0900

    test

--- server/index.js
+++ server/index.js
@@ -22,13 +22,13 @@ mongoose.connect(config.DB_URI, {
 
 app.use('/api/v1/products', productRoutes)
 
-if(process.env.NODE_ENV === 'production') {
+// if(process.env.NODE_ENV === 'production') {
   const appPath = path.join( __dirname, '..', 'dist', 'reservation-app4')
   app.use(express.static(appPath))
   app.get("*", function(req, res) {
     res.sendFile(path.resolve(appPath, 'index.html'))
   })
-}
+// }
 
 const PORT = process.env.PORT || '3001'
 
